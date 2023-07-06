df820018a933be56c87840c161d7728d6bc3c960
ghdcksgml1 <ghdcksgml2@naver.com>
2023-07-06 22:52:08 +0900

    refactor: BuilderFactory Deprecated 돼서 Repository로 변경

--- src/main/java/com/batch/app/job/SimpleJobConfiguration.java
+++ src/main/java/com/batch/app/job/SimpleJobConfiguration.java
@@ -4,26 +4,44 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
 public class SimpleJobConfiguration {
 
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
+    private final JobRepository jobRepository;
+
+    private final PlatformTransactionManager transactionManager;
+
+    private final TestTasklet tasklet;
 
     /**
      * Batch Job 생성
      */
     @Bean
     public Job simpleJob() {
-        return jobBuilderFactory.get("simpleJob")
+        return new JobBuilder("simpleJob", jobRepository)
                 .start(simpleStep1())
                 .build();
     }
@@ -33,12 +51,21 @@ public class SimpleJobConfiguration {
      */
     @Bean
     public Step simpleStep1() {
-        return stepBuilderFactory.get("simpleStep1")
-                .tasklet((contribution, chunkContext) -> { // 단일로 수행될 기능들을 명시
-                    log.info(">>>>>> This is Step1");
-
-                    return RepeatStatus.FINISHED;
-                })
+        return new StepBuilder("simpleStep1", jobRepository)
+                .tasklet(tasklet, transactionManager)
                 .build();
+    }
+
+    @StepScope
+    @Component
+    public class TestTasklet implements Tasklet {
+
+        @Override
+        public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+            log.info(">>>>>> This is Step1");
+
+            return RepeatStatus.FINISHED;
+        }
+
     }
 }
